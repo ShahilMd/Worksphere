d63622b0241b5891bcadd6beed1046ee0e691fd2
ShahilMd <shahil.connect@gmail.com>
2025-06-23 04:42:09 +0530

    chore: initial backend setup with TypeScript, Express, dotenv, and basic config

---
+++ backend/src/index.ts
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response, urlencoded } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import { use } from "passport";
+
+const app = express()
+
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }))
+
+app.use(
+  session({
+    name:"session",
+    keys:[config.SESSION_SECRET],
+    maxAge:24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite :"lax",
+  })
+)
+
+app.use(cors({
+  origin: config.FRONTEND_ORIGIN,
+  credentials: true,
+}))
+
+app.get('/',(req: Request,res: Response ,next: NextFunction)=>{
+  res.status(200).json({
+    message: " THis is home Route"
+  })
+})
+
+app.listen(config.PORT,async ()=>{
+  console.log(`Server is listing on port ${config.PORT}`)
+})
